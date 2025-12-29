41bffacb8c6a32f3b322890efc3e7ededd3c3346
baohonghui <baohonghui@kuaishou.com>
2025-12-29 21:27:59 +0800

    docs: hum

--- scripts/train_HUM.sh
+++ scripts/train_HUM.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+export NCCL_P2P_LEVEL=NVL
+export http_proxy=http://oversea-squid1.jp.txyun:11080 
+export https_proxy=http://oversea-squid1.jp.txyun:11080 
+export no_proxy=localhost,127.0.0.1,localaddress,localdomain.com,internal,corp.kuaishou.com,test.gifshow.com,staging.kuaishou.com
 
 # Parameters
 OUTPUT_PATH=${1:-./ckp/hum_v1.pth}
-DATASET=${2:-m_IOATBC}
+DATASET=${2:-mIndustrial_and_Scientific}
 LR=${3:-5e-5}
 CONFIG=${4:-configs/train_HUM.yaml}
 NUM_GPUS=${5:-8}
